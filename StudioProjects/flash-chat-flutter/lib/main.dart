b796eb7bc4d57a8c6c012b5f884f3bb24befec51
Shahkar Uzair <shahkar.uzair.3@gmail.com>
2022-05-16 20:35:45 +0500

    first commit 

--- StudioProjects/flash-chat-flutter/lib/main.dart
+++ StudioProjects/flash-chat-flutter/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:flash_chat/provider/google_sign_in.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,20 +18,22 @@ void main() async {
 
 class FlashChat extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          bodyText1: TextStyle(color: Colors.black54),
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => GoogleSignInProvider(),
+        child: MaterialApp(
+          theme: ThemeData.dark().copyWith(
+            textTheme: TextTheme(
+              bodyText1: TextStyle(color: Colors.black54),
+            ),
+          ),
+          initialRoute: WelcomeScreen.id,
+          routes: {
+            WelcomeScreen.id: (context) => WelcomeScreen(),
+            LoginScreen.id: (context) => LoginScreen(),
+            RegistrationScreen.id: (context) => RegistrationScreen(),
+            ChatScreen.id: (context) => ChatScreen(),
+          },
         ),
-      ),
-      initialRoute: WelcomeScreen.id,
-      routes: {
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
-        RegistrationScreen.id: (context) => RegistrationScreen(),
-        ChatScreen.id: (context) => ChatScreen(),
-      },
-    );
-  }
+        // designSize: const Size(360, 640),
+      );
 }
--- StudioProjects/flash-chat-flutter/lib/screens/welcome_screen.dart
+++ StudioProjects/flash-chat-flutter/lib/screens/welcome_screen.dart
@@ -1,9 +1,13 @@
+import 'package:flash_chat/provider/google_sign_in.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
 
-import '../components/rounded_button.dart';
+// import '../components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
@@ -69,19 +73,88 @@ class _WelcomeScreenState extends State<WelcomeScreen>
             SizedBox(
               height: 48.0,
             ),
-            RoundedButton(
-              title: 'login',
-              color: Colors.lightBlueAccent,
+            ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, LoginScreen.id);
               },
+              child: Text(
+                'Log In',
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.lightBlueAccent,
+                onPrimary: Colors.black,
+                padding: EdgeInsets.all(18.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
             ),
-            RoundedButton(
-              title: 'Register',
-              color: Colors.lightBlueAccent,
+            SizedBox(
+              height: 10.0,
+            ),
+
+            // RoundedButton(
+            //   title: 'Register',
+            //   color: Colors.lightBlueAccent,
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, RegistrationScreen.id);
+            //   },
+            // ),
+            ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, RegistrationScreen.id);
               },
+              child: Text('Sign Up'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.lightBlueAccent,
+                onPrimary: Colors.black,
+                padding: EdgeInsets.all(18.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+
+            Text(
+              "OR",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            // RoundedButton(
+            //   title: 'Log In with Gmail',
+            //   color: Colors.lightBlueAccent,
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, RegistrationScreen.id);
+            //   },
+            // ),
+            ElevatedButton.icon(
+              icon: FaIcon(
+                FontAwesomeIcons.google,
+                color: Colors.red,
+              ),
+              label: Text('Sign Up with Gmail'),
+              onPressed: () {
+                final provider =
+                    Provider.of<GoogleSignInProvider>(context, listen: false);
+                provider.googleLogin();
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.lightBlueAccent,
+                onPrimary: Colors.black,
+                padding: EdgeInsets.all(15.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
             ),
           ],
         ),
